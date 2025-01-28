edd61902d5f5488f279c3bb365d98d930a2f7a71
matthias314 <matthias314@posteo.net>
2025-01-28 15:37:53 -0500

    added TestModule

---
+++ TestModule/src/TestModule.jl
@@ -0,0 +1,45 @@
+"""
+    @__MODULE__
+
+This module collects several useful functions to deal with vectors.
+"""
+module TestModule
+
+export Vec, testsum
+
+using StaticArrays
+
+const T = Int8
+const N = 4
+
+"""
+    const Vec = SVector{$N,$T}
+
+This is the vector type used my $(@__MODULE__).
+"""
+const Vec = SVector{N,T}
+
+"""
+    testsum(v::Vec) -> $T
+
+Returns the sum of the elements in `v`. There is no check for overflow!
+
+See also [`Vec`](@ref).
+
+# Examples
+```julia
+julia> v = Vec([1, 2, 3]); testsum(v)
+6
+julia> v = Vec(1:20); testsum(v)
+-46
+```
+"""
+function testsum(v::Vec)
+    s = zero(T)
+    for x in v
+        s += x
+    end
+    return s
+end
+
+end # module
